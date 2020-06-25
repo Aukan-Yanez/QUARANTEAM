9b03183d36e436927bdb0210a327f1729722543a
Miguel Medina <50216704+ItsMiwel@users.noreply.github.com>
2020-06-25 03:27:27 -0400

    Pause Menu J1 transiciones
    Creacion del menu de pause para el J1 con transiciones en los botones principales (hasta el momento 90% funcional)

--- Quaranteam/Assets/J1/Scriptss/PauseMenu.cs
+++ Quaranteam/Assets/J1/Scriptss/PauseMenu.cs
@@ -1,28 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
 
     public GameObject pauseMenuUI;
-    public GameObject pauseButton;
+    public GameObject settingsMenuUI;
+    public GameObject exitMenuUI;
+
+    public GameObject Logo;
+    public GameObject Gral_Resume;
+    public GameObject Gral_Settings;
+    public GameObject Gral_Exit;
+
+    private Animator Logo_Anim;
+    private Animator Gral_Resume_Anim;
+    private Animator Gral_Sett_Anim;
+    private Animator Gral_Exit_Anim;
+
+    void Update()
+    {
+        string resumen = Gral_Resume_Anim.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        string settings = Gral_Resume_Anim.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        string exit = Gral_Resume_Anim.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        if (resumen == "Highlighted" || settings == "Highlighted" || exit == "Highlighted")
+        {
+            Gral_Resume_Anim.SetBool("Close", true); /**/
+            Gral_Sett_Anim.SetBool("Open", true);
+            Gral_Exit_Anim.SetBool("Open", true);
+        }
+    }
+
+
+    private void Awake()
+    {
+        Gral_Resume_Anim = Gral_Resume.GetComponent<Animator>();
+        Gral_Sett_Anim = Gral_Settings.GetComponent<Animator>();
+        Gral_Exit_Anim = Gral_Exit.GetComponent<Animator>();
+        Logo_Anim = Logo.GetComponent<Animator>();
+    }
 
     public void Pause()
     {
-        Debug.Log("PAUSE");
+        //Debug.Log("PAUSE");
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
+
+        Logo_Anim.Play("PauseLogo");
+        Gral_Resume_Anim.Play("Resume");
+        Gral_Resume_Anim.SetBool("Open", true);/**/
+        Gral_Sett_Anim.Play("Settings");
+        Gral_Exit_Anim.Play("Exit");
+
+        
     }
 
     public void Resume()
     {
-        Debug.Log("RESUMEN");
-        pauseMenuUI.SetActive(false);
+        //Debug.Log("RESUMEN");
+        //pauseMenuUI.SetActive(false);
+
+        Logo_Anim.Play("PauseLogo2");
+        Gral_Resume_Anim.Play("Resume2");
+        Gral_Resume_Anim.SetBool("Open", false); /**/
+        Gral_Sett_Anim.Play("Settings2");
+        Gral_Exit_Anim.Play("Exit2");
         Time.timeScale = 1f;
     }
 
-    
+    public void openAnim()
+    {
+        Gral_Resume_Anim.SetBool("Close", true);/**/
+        Gral_Resume_Anim.SetBool("Open", true);
+        Gral_Sett_Anim.SetBool("Open", true);
+        Gral_Exit_Anim.SetBool("Open", true);
+    }
+
     public void LoadMenu()
     {
         Debug.Log("Loading menu...");
@@ -30,7 +87,8 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Quitting game...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    
 
 }
---
+++ Quaranteam/Assets/Menu/Scripts/startAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class startAnimation : MonoBehaviour
+{
+    public Button ButtonJ1;
+    public Button ButtonJ2;
+    public Button ButtonJ3;
+    public Button ButtonStart;
+
+    bool activateJ1 = false;
+    bool activateJ2 = false;
+    bool activateJ3 = false;
+
+    void Update()
+    {
+        string J1 = ButtonJ1.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        string J2 = ButtonJ2.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        string J3 = ButtonJ3.gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        if (J1 == "Highlighted" && !activateJ1)
+        {
+            ButtonStart.gameObject.GetComponent<Animator>().SetTrigger("StartJ1");
+            activateJ1 = true;
+            activateJ2 = false;
+            activateJ3 = false;
+
+        }
+        if (J2 == "Highlighted" && !activateJ2)
+        {
+            ButtonStart.gameObject.GetComponent<Animator>().SetTrigger("StartJ2");
+            activateJ1 = false;
+            activateJ2 = true;
+            activateJ3 = false;
+        }
+        if (J3 == "Highlighted" && !activateJ3)
+        {
+            ButtonStart.gameObject.GetComponent<Animator>().SetTrigger("StartJ3");
+            activateJ1 = false;
+            activateJ2 = false;
+            activateJ3 = true;
+        }
+        if (J1 == "Normal" && J2 == "Normal" && J3 == "Normal")
+        {
+            ButtonStart.gameObject.GetComponent<Animator>().SetTrigger("Normal");
+            activateJ1 = false;
+            activateJ2 = false;
+            activateJ3 = false;
+        }
+    }
+}
